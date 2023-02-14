68c84c72add72b34a77936faf31330193d9572b3
Chrishan Jayakody <chrishan1989@gmail.com>
2023-02-14 12:14:31 +0530

    Added validation using express-validator library

--- users/users.routes.config.ts
+++ users/users.routes.config.ts
@@ -1,7 +1,9 @@
 import { CommonRoutesConfig } from '../common/common.routes.config';
 import UsersController from './controllers/users.controllers';
 import UsersMiddleware from './middleware/users.middleware';
+import BodyValidationMiddleware from '../common/middleware/body.validation.middleware';
 import express from 'express';
+import { body } from 'express-validator';
 
 export class UserRoutes extends CommonRoutesConfig {
     constructor(app: express.Application) {
@@ -14,7 +16,11 @@ export class UserRoutes extends CommonRoutesConfig {
             .route('/user')
             .get(UsersController.listUsers)
             .post(
-                UsersMiddleware.validateRequiredUserBodyFields,
+                body('email').isEmail(),
+                body('password')
+                    .isLength({ min: 6 })
+                    .withMessage('Must include password (6+ characters'),
+                BodyValidationMiddleware.verifyBodyFieldsErrors,
                 UsersMiddleware.validateSameEmailDoesntExist,
                 UsersController.createUser
             );
@@ -30,13 +36,29 @@ export class UserRoutes extends CommonRoutesConfig {
 
         this.app
             .put('/users/:userId', [
-                UsersMiddleware.validateRequiredUserBodyFields,
+                body('email').isEmail(),
+                body('password')
+                    .isLength({ min: 6 })
+                    .withMessage('Must include password (6+ characters)'),
+                body('firstName').isString(),
+                body('lastName').isString(),
+                body('permissionFlags').isInt(),
+                BodyValidationMiddleware.verifyBodyFieldsErrors,
                 UsersMiddleware.validateSameEmailBelongsToSameUser,
                 UsersController.put
             ]);
 
         this.app
             .patch('/users/:userId', [
+                body('email').isEmail().optional(),
+                body('password')
+                    .isLength({ min: 6 })
+                    .withMessage('Password must be 6+ characters')
+                    .optional(),
+                body('firstName').isString().optional(),
+                body('lastName').isString().optional(),
+                body('permissionFlags').isInt().optional(),
+                BodyValidationMiddleware.verifyBodyFieldsErrors,
                 UsersMiddleware.validatePatchEmail,
                 UsersController.patch
             ]);
