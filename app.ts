959c5a9608e007f23727890bb1d5024a51124b27
Chrishan Jayakody <chrishan1989@gmail.com>
2023-02-17 14:37:01 +0530

    Added Helmet and Docker

--- app.ts
+++ app.ts
@@ -5,6 +5,7 @@ if (dotenvResult.error) {
 }
 import express from 'express';
 import * as http from 'http';
+import helmet from 'helmet';
 
 import * as winston from 'winston';
 import * as expressWinston from 'express-winston';
@@ -25,6 +26,7 @@ const debugLog: debug.IDebugger = debug('app');
 
 app.use(express.json());
 app.use(cors());
+app.use(helmet());
 
 const loggerOptions: expressWinston.LoggerOptions = {
     transports: [new winston.transports.Console()],
